7448c43975906208b7c3cde67b443444ed826ab6
Muhammed Anas <muhammedanas4140@gmail.com>
2017-12-26 10:45:51 +0530

    fs module advanced

---
+++ fs/app.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+
+// // console.log(fs.readFileSync('readMe.txt', 'utf8'));
+
+// // write to another file
+// // fs.writeFileSync('newFile.txt',fs.readFileSync('readMe.txt', 'utf8'));
+
+// // Asynchtounous
+
+// fs.readFile('readMe.txt', 'utf8', (err, data) => {
+// 	if(err) {
+// 		console.log(err);
+// 	}
+// 	fs.writeFile('writefile.txt', data , (err) => {
+// 		if(err) throw err;
+// 	});
+// })
+// fs.unlink('deleteMe.txt', (err) => {
+// 	if(err) console.log(err);
+// 	console.log('successfully deleted')
+// })
+// console.log('first');
+
+// // # Make & Remove Directory
+// 	//sync
+ 
+ // fs.mkdirSync('newDir')
+ // fs.rmdirSync('newDir');
+
+// Create a directory and a copy file into inner directery file.
+	fs.mkdir('app', (err) => {
+		if(err) throw errr
+		fs.readFile('readMe.txt', 'utf8', (err, data) => {
+			if(err) throw err;
+			fs.writeFile('./app/newFile.txt', data, (err) => {
+				if(err) throw err;
+				console.log('file copied')
+			})
+		})
+	})
