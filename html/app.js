89ada262eb489f17d3bbcd34b730304fa5b7490f
Muhammed Anas <muhammedanas4140@gmail.com>
2017-12-28 17:15:55 +0530

    Serveing html using with node pipes

---
+++ html/app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const fs = require('fs')
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'})
+	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+	myReadStream.pipe(res)
+})
+server.listen(8000)
