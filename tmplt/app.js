2e778f84e4ce10d92f13ae784e51f08b22adffb5
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-01 11:47:01 +0530

    Express render and template Engines

---
+++ tmplt/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express()
+	app.set('view engine', 'ejs')
+
+	app.get('/', (req,res) => {
+		res.sendFile(__dirname + '/views/index.html');
+	})
+	app.get('/contact', (req, res) => {
+		res.sendFile(__dirname + '/views/contact.html')
+	})
+	app.get('/profile/:name', (req, res) => {
+		const data = {
+			age: 22,
+			job: 'Web Developer'
+		}
+		res.render('profile', {person: req.params.name, data: data})
+	})
+app.listen(8000, () => console.log('server on port 8000'))
