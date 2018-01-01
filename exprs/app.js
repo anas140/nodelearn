64afe84a561a325cad3e391a2ebb64a5c6a48972
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-01 11:15:04 +0530

    Route Parameters

--- exprs/app.js
+++ exprs/app.js
@@ -7,4 +7,9 @@ app.get('/', (req,res) => {
 app.get('/contact', (req,res) => {
 	res.send('Contact Page')
 })
+// Route Parameters
+app.get('/profile/:id', (req,res) => {
+	res.send('You requested the id with ' + req.params.id)
+})
+
 app.listen(8000, () => console.log('app listened on port 8000'))
