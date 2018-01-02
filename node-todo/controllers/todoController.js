183a28719b5b7966dd73ee5c69511a74695ef7a3
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-02 12:43:29 +0530

    Added todos manually

--- node-todo/controllers/todoController.js
+++ node-todo/controllers/todoController.js
@@ -1,6 +1,7 @@
+var data = [{item: 'Get money'}, {item: 'complete nodejs basic'}, {item: 'learn mysql'}, {item: 'learn middleware'}, {item: 'learn rest api'}, {item: 'learn jwt'}, {item: 'learn oauth'}, {item: 'learn react'}, {item: 'learn querys mysl'}, {item: 'learn mysql normalisation'}]
 module.exports = function(app) {
 	app.get('/todo', (req, res) => {
-		res.render('todo')
+		res.render('todo', {todos: data})
 	})
 	app.post('/todo', (req, res) => {
 		console.log('post todo')
