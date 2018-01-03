585a84da288862d2480a6bde16be337802f777e8
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-03 11:11:56 +0530

    Mongo db integration and Model creation

--- node-todo/controllers/todoController.js
+++ node-todo/controllers/todoController.js
@@ -1,4 +1,25 @@
 const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose')
+
+// Connect db
+mongoose.connect('mongodb://anas:asd123.@ds239097.mlab.com:39097/todo',(err) => {
+	if (err) console.log(err) 
+})
+
+// Define a Todo Schema and Model
+// Schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+	item: String
+})
+
+// Model
+var Todo = mongoose.model('Todo', todoSchema)
+var item1 = Todo({item: 'buy flowers'}).save(function(err) {
+	if(err) console.log(err)
+	console.log('item saved')
+})
+
 var data = [{item: 'Get money'}, {item: 'complete nodejs basic'}, {item: 'learn mysql'}, {item: 'learn middleware'}, {item: 'learn rest api'}, {item: 'learn jwt'}, {item: 'learn oauth'}, {item: 'learn react'}, {item: 'learn querys mysl'}, {item: 'learn mysql normalisation'}]
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 module.exports = function(app) {
