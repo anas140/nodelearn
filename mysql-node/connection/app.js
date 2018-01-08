adc818aafe0ea8c9768d2bb301aaa25d81855048
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-08 23:02:50 +0530

    Added Routes

--- mysql-node/connection/app.js
+++ mysql-node/connection/app.js
@@ -4,9 +4,21 @@ let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
+    database: 'nodedb'
 })
 
 connection.connect((err) => {
     if(err) throw err
     console.log('Connected as id' + connection.threadId)
+    // Create a databse using mysql Query
+    // connection.query('CREATE DATABASE nodedb', (err, result) => {
+    //     if(err) console.log(err)
+    //     // console.log(result)
+    // })
+
+    // Create a table using mysql
+    // connection.query('CREATE TABLE customers (name varchar(255), address varchar(255))', (error, results, fields) => {
+    //     if(error) throw error.message
+    //     console.log('table created')
+    // })
 })
